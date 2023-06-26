36537633a80ec98d9b36bca304e61468136e2e21
Shivam Kansagara <202001086@daiict.ac.in>
2023-06-26 15:40:44 +0530

    adding about page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './MyComponents/Navbar';
 import Carousel from './MyComponents/Carousel';
 import Accordion from './MyComponents/Accordion';
+import About from './MyComponents/About';
+
 function App() {
   return (
     <>
     <div >
-      <div  style={{ backgroundColor: "#000000" }}>
-      <Navbar/>
-      <br />
-      <Carousel/>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={
+      <div>
+              <div style={{ backgroundColor: '#000000' }}>
+              <Carousel />
+              </div>
+              <div style={{ backgroundColor: '#FFFF00' }}>
+              <Accordion />
+              </div>
       </div>
-      <div style={{ backgroundColor: "#000000" }}>
-      <Accordion/>
-      </div>
+            
+          } />
+      <Route exact path="/about" element={<About/>} />
+    </Routes>
     </div>
     </>
+    
+  
   );
 }
 
--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -1,47 +1,49 @@
 import React from 'react'
-
+import "./Underlinecomp.css"
+import { Route,Routes,Link } from 'react-router-dom';
+import About from './About';
 export default function Navbar() {
     let backnavcolor={
         backgroundColor: "#000000"
     };
   return (
+    <>
+    
     <div >
         <nav className="navbar sticky-top navbar-expand-lg " style={backnavcolor}>
   <div className="container-fluid">
-    <a className="navbar-brand text-light" href="#">Navbar</a>
+    <a className="navbar-brand text-light underline-on-hover" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse " id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+          <a className="nav-link active text-light underline-on-hover"   aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-light" href="#">About</a>
+          <Link className="nav-link text-light underline-on-hover" to="/about">About</Link>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item text-light"  href="#">Action</a></li>
-            <li><a className="dropdown-item text-light" href="#">Another action</a></li>
+            <li><a className="dropdown-item text-dark underline-on-hover"  href="#">Action</a></li>
+            <li><a className="dropdown-item text-dark underline-on-hover" href="#">Another action</a></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item text-light" href="#">Something else here</a></li>
+            <li><a className="dropdown-item text-dark underline-on-hover" href="#">Something else here</a></li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled text-light">Disabled</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-info bg-light text-dark" type="submit">Search</button>
+        <button className="btn btn-outline-info bg-light text-dark " type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
     </div>
+   </>
   )
 }
