e48afac60a6e5928d6c00ffb6cfb1343e25c1db1
Shivam Kansagara <202001086@daiict.ac.in>
2023-07-07 01:12:24 +0530

    adding login page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './MyComponents/Navbar';
-import Carousel from './MyComponents/Carousel';
-import About from './MyComponents/About';
-import ShowCard from './MyComponents/ShowCard';
-import Content from './MyComponents/Content';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./MyComponents/Navbar";
+import Carousel from "./MyComponents/Carousel";
+import About from "./MyComponents/About";
+import ShowCard from "./MyComponents/ShowCard";
+import Content from "./MyComponents/Content";
 import Footer from "./MyComponents/Footer";
+import LoginSignUp from "./MyComponents/LoginSignUp";
+
 // #c5d5cb
 // #9fa8a3
 // #e3e0cf
 
-
 function App() {
   return (
     <>
-    <div >
-    <Navbar/> 
-    <Routes>
-    <Route exact path="/" element={
       <div>
-              <div style={{ backgroundColor: '#000000' }}>
-              <Carousel />
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <div>
+                <div style={{ backgroundColor: "#000000" }}>
+                  <Carousel />
+                </div>
+                <div style={{ backgroundColor: "#000000" }}>
+                  <Content />
+                </div>
               </div>
-              <div style={{ backgroundColor: '#000000' }}>
-              <Content />
-              </div>
+            }
+          />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/ShowProducts" element={<ShowCard />} />
+          <Route exact path="/Login_SignUp" element={<LoginSignUp />}></Route>
+        </Routes>
+        <footer>
+          <Footer />
+        </footer>
       </div>
-            
-    } />
-      <Route exact path="/about" element={<About/>} />
-      <Route exact path="/ShowProducts" element={<ShowCard/>}/>
-    </Routes>
-    <footer>
-    <Footer/>
-    </footer>
-    </div>
     </>
   );
 }
--- Frontend/src/MyComponents/Navbar.js
+++ Frontend/src/MyComponents/Navbar.js
@@ -1,48 +1,121 @@
-import React from 'react'
-import "./Underlinecomp.css"
-import { Route,Routes,Link } from 'react-router-dom';
+import React from "react";
+import "./Underlinecomp.css";
+import { Route, Routes, Link } from "react-router-dom";
 export default function Navbar() {
-    let backnavcolor={
-        backgroundColor: "#c5d5cb"
-    };
+  let backnavcolor = {
+    backgroundColor: "#c5d5cb",
+  };
   return (
     <>
-    
-    <div >
-        <nav className="navbar fixed-top navbar-expand-lg " style={backnavcolor}>
-  <div className="container-fluid">
-    <Link className="navbar-brand text-light underline-on-hover text-dark font-weight-bold"  to="/">PriceHub</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse " id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active text-light underline-on-hover text-dark"   aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-light underline-on-hover text-dark" to="/about">About</Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle text-light text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item text-dark underline-on-hover"  href="#">Action</a></li>
-            <li><a className="dropdown-item text-dark underline-on-hover" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item text-dark underline-on-hover" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <Link to="/ShowProducts"><button className="btn btn-dark" type="submit">Click here to Search</button></Link>
-        
-      </form>
-    </div>
-  </div>
-</nav>
-    </div>
-   </>
-  )
+      <div>
+        <nav
+          className="navbar fixed-top navbar-expand-lg "
+          style={backnavcolor}
+        >
+          <div className="container-fluid">
+            <Link
+              className="navbar-brand text-light underline-on-hover text-dark font-weight-bold"
+              to="/"
+            >
+              PriceHub
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse "
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active text-light underline-on-hover text-dark"
+                    aria-current="page"
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-light underline-on-hover text-dark"
+                    to="/about"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle text-light text-dark"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a
+                        className="dropdown-item text-dark underline-on-hover"
+                        href="#"
+                      >
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item text-dark underline-on-hover"
+                        href="#"
+                      >
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item text-dark underline-on-hover"
+                        href="#"
+                      >
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <form className="d-flex mr-3" role="search">
+                  <Link to="/ShowProducts">
+                    <button className="btn btn-dark" type="submit">
+                      Click here to Search
+                    </button>
+                  </Link>
+                </form>
+              </ul>
+
+              <ul className="navbar-nav mr-3 mb-2 mb-lg-0">
+                <li className="nav-item d-flex">
+                  <Link
+                    className="nav-link active text-dark"
+                    aria-current="page"
+                    to="/Login_SignUp"
+                  >
+                    <button className="btn btn-primary">Login/SignUp</button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
